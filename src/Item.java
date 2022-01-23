7040db0b86922709cfc1a89f4f7f4bfd700d8a25
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2022-01-22 17:08:31 -0700

    First push of Project

---
+++ src/Item.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Item extends JPanel {
+    private static JDialog d;
+    JTextField t1, t2;
+    String s1, s2;
+
+    public Item() {
+
+    }
+
+    public JPanel makepanel() {
+        JPanel panel = new JPanel();
+
+        // panel.setLayout(new BoxLayout(panel, 0));
+        panel.setLayout(new GridLayout());
+        // panel.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel.add(new JLabel("Item Name: "));
+        panel.add(new JLabel(s1));
+        panel.add(new JLabel("Item Quantity: "));
+        panel.add(new JLabel(s2));
+        panel.setBorder(new EmptyBorder(new Insets(15, 15, 15, 15)));
+        JButton delete = new JButton("Delete");
+        delete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                panel.setVisible(false);
+            }
+        });
+        JButton plus1 = new JButton("+1");
+        plus1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("jksjfds");
+            }
+        });
+        JButton minus1 = new JButton("-1");
+        minus1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("jksjfds");
+            }
+        });
+
+        panel.setMaximumSize(new Dimension(900, 50));
+        delete.setMinimumSize(new Dimension(150, 25));
+        panel.add(plus1);
+        panel.add(minus1);
+        panel.add(delete);
+        return panel;
+    }
+
+    public void getInfo(JFrame frame) {
+        d = new JDialog(frame, "Dialog Example", true);
+        d.setLayout(new FlowLayout());
+
+        JButton b = new JButton("OK");
+        t1 = new JTextField("Product Name");
+        t2 = new JTextField("Quantity");
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                s1 = t1.getText();
+                s2 = t2.getText();
+
+                Item.d.setVisible(false);
+            }
+        });
+        d.add(t1);
+        d.add(t2);
+
+        d.add(b);
+        d.setSize(300, 300);
+        d.setVisible(true);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class Main implements ActionListener {
+    JScrollPane scrlpane;
+    ArrayList<JPanel> items = new ArrayList<JPanel>();
+    JPanel masterPanel = new JPanel();
+    JFrame masterFrame;
+    JScrollPane jScrollPane;
+    int uniqueItems;
+
+    public static void main(String args[]) {
+        new Main();
+    }
+
+    public Main() {
+        // Init the frame
+        JFrame frame = new JFrame("Tabbed Pane Sample");
+        masterFrame = frame;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Init the scrolling main section
+        JLabel label = new JLabel("Label");
+        label.setPreferredSize(new Dimension(1000, 1000));
+        jScrollPane = new JScrollPane(label);
+
+        // Init the add info button
+        JButton newItemBtn = new JButton("New Item");
+        newItemBtn.addActionListener(this);
+
+        JButton deleteAll = new JButton("Delete All");
+        deleteAll.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                masterPanel.removeAll();
+                masterPanel.revalidate();
+                masterPanel.repaint();
+            }
+        });
+        masterFrame.add(deleteAll, BorderLayout.SOUTH);
+        // to stores the ratings (1 stat, 2 star, etc)
+
+        masterFrame.add(newItemBtn, BorderLayout.NORTH);
+
+        jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jScrollPane.setViewportBorder(new LineBorder(Color.RED));
+        jScrollPane.setBorder(new EmptyBorder(25, 25, 25, 25));
+        jScrollPane.getViewport().add(scrlpane, BorderLayout.CENTER);
+
+        masterFrame.add(jScrollPane, BorderLayout.CENTER);
+        masterFrame.setSize(1200, 750);
+        masterFrame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        this.update();
+    }
+
+    public void update() {
+
+        uniqueItems += 1;
+
+        Item item = new Item();
+        item.getInfo(masterFrame);
+
+        masterPanel.add(item.makepanel());
+        // masterFrame.getContentPane().add(scrlpane, BorderLayout.CENTER);
+        masterPanel.setLayout(new BoxLayout(masterPanel, BoxLayout.PAGE_AXIS));
+
+        scrlpane = new JScrollPane(masterPanel);
+        jScrollPane.getViewport().add(scrlpane, BorderLayout.CENTER);
+
+        // masterFrame.revalidate();
+        // masterFrame.repaint();
+        // masterFrame.setVisible(true);
+    }
+}
